d128b658713a1ef6be366da40f2bb2c6362db0b3
kelvinmburu <mburuhkelvin@gmail.com>
2022-04-26 16:19:46 +0300

    build: imported all properties

---
+++ app/__init__.py
@@ -0,0 +1,9 @@
+from app import views
+from flask import Flask
+from flask_bootstrap import Bootstrap
+
+# Initializing application
+app = Flask(__name__)
+
+# Initializing Flask Extensions
+bootstrap = Bootstrap(app)
